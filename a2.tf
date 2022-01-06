87d1c100add694aee6564e49f329a01e77d2d16a
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-06 08:48:30 +0530

    Update a2.tf

--- a2.tf
+++ a2.tf
@@ -1,7 +1,32 @@
 
 provider "aws" {
- profile = "default"
   region = "us-east-2"
+}
+resource "aws_lb" "test" {
+  name               = var.name_lb
+  internal           = false
+  load_balancer_type = var.load_balancer_type
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+}
+depends_on =[ 
+    aws_launch_template.alt.id
+ ]
+
+resource "aws_lb_target_group" "test" {
+  name     = var.name_TG
+  port     = 80
+  protocol = var.protocol
+  vpc_id   = var.vpc_id
+}
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = var.protocol
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
 }
   resource "aws_launch_template" "alt" {
   name_prefix   = "alt"
